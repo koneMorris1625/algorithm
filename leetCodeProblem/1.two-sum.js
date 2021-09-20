29f5e828f21d15748410fdb33dddca9aa8cadfa0
koneMorris1625 <reolcharm@gmail.com>
2021-09-20 22:28:59 +0800

    add leetcode #1 two sum

---
+++ leetCodeProblem/1.two-sum.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+    // let hash = {};
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (hash[target - nums[i]] !== undefined) { // Xa = nums[i], Xb = target - Xa;
+    //         return [hash[target - nums[i]], i];
+    //     }
+    //     hash[nums[i]] = i;
+    // }
+    // return [];
+    // --------------------------------------
+    // let hash = new Map();
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     const e = nums[i];
+    //     if (hash.has(target - e)) {
+    //         return [hash.get(target - e), i];
+    //     }
+    //     hash.set(e, i);
+    // }
+
+    var l = 0, r = nums.length - 1;
+    var res = [];
+    nums.sort((a, b) => a - b);
+    while (l < r) {
+        if (nums[l] + nums[r] === target) {
+            res.push(l);
+            res.push(r);
+            return res;
+        }
+        if (nums[l] + nums[r] < target) {
+            l++;
+        }
+        if (nums[l] + nums[r] > target) {
+            r--;
+        }
+    }
+    return res;
+
+}
+
+// @lc code=end
